b23765aa71a0e5c2fefa346bd03efade26dc8d66
logmar98 <marwanarbib15@gmail.com>
2025-06-19 15:52:44 +0100

    update the api keys

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore, initializeFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCH86cQ-W2W501W3-tmUB_EVSDv7wYA_54",
-  authDomain: "ecommerce-d019e.firebaseapp.com",
-  projectId: "ecommerce-d019e",
-  storageBucket: "ecommerce-d019e.firebasestorage.app",
-  messagingSenderId: "113989765309",
-  appId: "1:113989765309:web:76626ad365d8321222cfd4",
-  measurementId: "G-Q5DF262DZW"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import './styles/global.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
 
